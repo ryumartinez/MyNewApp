b959f72c2d9c927fa3a86de8cfc49da54a04e1a7
Ryu Martinez <ryueduardomartinez@outlook.es>
2025-12-21 02:14:35 -0300

    @lovesworking/watermelondb-expo-plugin-sdk-52-plus

---
+++ react-native.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    project: {
+        android: {
+            sourceDir: './android',
+            packageName: 'com.anonymous.MyNewApp', // Ensure this matches your app.json
+        },
+        ios: {},
+    },
+};
